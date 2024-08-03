e09a9c8948616fbbf44dff721b8a0178f005c36b
Juri Pospelow <juri@pospelow.de>
2024-08-03 16:33:36 +0200

    webapi: introduce WebServer::start()

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -7,6 +7,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 
+#include "Listener.hpp"
 #include "WebServer.hpp"
 
 using namespace std;
@@ -61,10 +62,10 @@ WebServer::WebServer(std::string fileName)
 
         read_ini(fileName, config);
         file_name = config.get<std::string>("input.log_file");
-        this->_address = net::ip::make_address(config.get<std::string>("net.pc_addr"));
-        this->_port = config.get<unsigned short>("net.api_port");
-        this->_doc_root = std::make_shared<std::string>(config.get<std::string>("net.doc_root"));
-        this->_threads = config.get<int>("opt.threads");
+        _address = net::ip::make_address(config.get<std::string>("net.pc_addr"));
+        _port = config.get<unsigned short>("net.api_port");
+        _doc_root = std::make_shared<std::string>(config.get<std::string>("net.doc_root"));
+        _threads = config.get<int>("opt.threads");
 
         // std::cout << "opt.threads" << ": " << config.get<std::string>("opt.threads") <<endl;
         // std::cout << "input.log_file" << ": " << config.get<std::string>("input.log_file") <<endl;
@@ -81,4 +82,44 @@ WebServer::WebServer(std::string fileName)
     }
 
     readCSV(file_name);
+}
+
+void WebServer::start(){
+
+    // The io_context is required for all I/O
+    net::io_context ioc{_threads};
+
+    // Create and launch a listening port
+    std::make_shared<listener>(
+        ioc,
+        tcp::endpoint{_address, _port},
+        _doc_root)->run();
+
+    // Capture SIGINT and SIGTERM to perform a clean shutdown
+    net::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait(
+        [&](beast::error_code const&, int)
+        {
+            // Stop the `io_context`. This will cause `run()`
+            // to return immediately, eventually destroying the
+            // `io_context` and all of the sockets in it.
+            ioc.stop();
+        });
+
+    // Run the I/O service on the requested number of threads
+    std::vector<std::thread> v;
+    v.reserve(_threads - 1);
+    for(auto i = _threads - 1; i > 0; --i)
+        v.emplace_back(
+        [&ioc]
+        {
+            ioc.run();
+        });
+    ioc.run();
+
+    // (If we get here, it means we got a SIGINT or SIGTERM)
+
+    // Block until all the threads exit
+    for(auto& t : v)
+        t.join();
 }
--- src/webapi.cpp
+++ src/webapi.cpp
@@ -4,7 +4,6 @@
 
 #include "WebServer.hpp"
 #include "HTTPSession.hpp"
-#include "Listener.hpp"
 
 
 int main(int argc, char* argv[])
@@ -22,10 +21,9 @@ int main(int argc, char* argv[])
     }
 
     WebServer web(fileName);
-#if 0
+    web.start();
 
-
-
+    return EXIT_SUCCESS;
 
     // MultiMap::iterator itr;
     // for (itr = DataPrepare.begin(); itr != DataPrepare.end(); ++itr) {
@@ -34,61 +32,7 @@ int main(int argc, char* argv[])
     //     cout << out << '\n';
     // }
 
-   // Check command line arguments.
-    if (argc != 5)
-    {
-        std::cerr <<
-            "Usage: webapi <address> <port> <doc_root> <threads>\n" <<
-            "Example:\n" <<
-            "    webapi 0.0.0.0 8080 . 1\n";
-        return EXIT_FAILURE;
-    }
-    auto const address = net::ip::make_address(argv[1]);
-    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-    auto const doc_root = std::make_shared<std::string>(argv[3]);
-    auto const threads = std::max<int>(1, std::atoi(argv[4]));
-
-    // The io_context is required for all I/O
-    net::io_context ioc{threads};
-
-    // Create and launch a listening port
-    std::make_shared<listener>(
-        ioc,
-        tcp::endpoint{address, port},
-        doc_root)->run();
-
-    // Capture SIGINT and SIGTERM to perform a clean shutdown
-    net::signal_set signals(ioc, SIGINT, SIGTERM);
-    signals.async_wait(
-        [&](beast::error_code const&, int)
-        {
-            // Stop the `io_context`. This will cause `run()`
-            // to return immediately, eventually destroying the
-            // `io_context` and all of the sockets in it.
-            ioc.stop();
-        });
-
-    // Run the I/O service on the requested number of threads
-    std::vector<std::thread> v;
-    v.reserve(threads - 1);
-    for(auto i = threads - 1; i > 0; --i)
-        v.emplace_back(
-        [&ioc]
-        {
-            ioc.run();
-        });
-    ioc.run();
-
-    // (If we get here, it means we got a SIGINT or SIGTERM)
-
-    // Block until all the threads exit
-    for(auto& t : v)
-        t.join();
-#endif
-    return EXIT_SUCCESS;
     // readData4Log(file);
     // startWebServer();
 
-
-    // return 0;
 }
