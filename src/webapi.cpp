cd66f9177e6dc887ccb38fc685eb2e5ab294eac1
Juri Pospelow <juri@pospelow.de>
2024-08-03 13:52:15 +0200

    webapi: introduce Listener.cpp

--- src/webapi.cpp
+++ src/webapi.cpp
@@ -1,146 +1,10 @@
-#include <iostream>     // cout, endl
-
-#include "WebServer.hpp"
-#include "HTTPSession.hpp"
-
-// ------------------------------------------------------
-
-#include <boost/beast/websocket.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/bind_executor.hpp>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/signal_set.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/make_unique.hpp>
-#include <boost/optional.hpp>
-#include <algorithm>
-#include <cstdlib>
-#include <functional>
 #include <iostream>
-
-#include <string>
 #include <thread>
 #include <vector>
 
-// ------------------------------------------------------
-
-
-
-
-namespace websocket = beast::websocket;         // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;                    // from <boost/asio.hpp>
-
-// ------------------------------------------------------
-
-// Report a failure
-void
-fail(beast::error_code ec, char const* what)
-{
-    std::cerr << what << ": " << ec.message() << "\n";
-}
-
-// Accepts incoming connections and launches the sessions
-class listener : public std::enable_shared_from_this<listener>
-{
-    net::io_context& ioc_;
-    tcp::acceptor acceptor_;
-    std::shared_ptr<std::string const> doc_root_;
-
-public:
-    listener(
-        net::io_context& ioc,
-        tcp::endpoint endpoint,
-        std::shared_ptr<std::string const> const& doc_root)
-        : ioc_(ioc)
-        , acceptor_(net::make_strand(ioc))
-        , doc_root_(doc_root)
-    {
-        beast::error_code ec;
-
-        // Open the acceptor
-        acceptor_.open(endpoint.protocol(), ec);
-        if(ec)
-        {
-            fail(ec, "open");
-            return;
-        }
-
-        // Allow address reuse
-        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
-        if(ec)
-        {
-            fail(ec, "set_option");
-            return;
-        }
-
-        // Bind to the server address
-        acceptor_.bind(endpoint, ec);
-        if(ec)
-        {
-            fail(ec, "bind");
-            return;
-        }
-
-        // Start listening for connections
-        acceptor_.listen(
-            net::socket_base::max_listen_connections, ec);
-        if(ec)
-        {
-            fail(ec, "listen");
-            return;
-        }
-    }
-
-    // Start accepting incoming connections
-    void
-    run()
-    {
-        // We need to be executing within a strand to perform async operations
-        // on the I/O objects in this session. Although not strictly necessary
-        // for single-threaded contexts, this example code is written to be
-        // thread-safe by default.
-        net::dispatch(
-            acceptor_.get_executor(),
-            beast::bind_front_handler(
-                &listener::do_accept,
-                this->shared_from_this()));
-    }
-
-private:
-    void
-    do_accept()
-    {
-        // The new connection gets its own strand
-        acceptor_.async_accept(
-            net::make_strand(ioc_),
-            beast::bind_front_handler(
-                &listener::on_accept,
-                shared_from_this()));
-    }
-
-    void
-    on_accept(beast::error_code ec, tcp::socket socket)
-    {
-        if(ec)
-        {
-            fail(ec, "accept");
-        }
-        else
-        {
-            // Create the http session and run it
-            std::make_shared<http_session>(
-                std::move(socket),
-                doc_root_)->run();
-        }
-
-        // Accept another connection
-        do_accept();
-    }
-};
-
-//------------------------------------------------------------------------------
-
-
+#include "WebServer.hpp"
+#include "HTTPSession.hpp"
+#include "Listener.hpp"
 
 
 int main(int argc, char* argv[])
@@ -158,9 +22,9 @@ int main(int argc, char* argv[])
     if (argc != 5)
     {
         std::cerr <<
-            "Usage: advanced-server <address> <port> <doc_root> <threads>\n" <<
+            "Usage: webapi <address> <port> <doc_root> <threads>\n" <<
             "Example:\n" <<
-            "    advanced-server 0.0.0.0 8080 . 1\n";
+            "    webapi 0.0.0.0 8080 . 1\n";
         return EXIT_FAILURE;
     }
     auto const address = net::ip::make_address(argv[1]);
