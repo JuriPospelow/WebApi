9e6a62b221ca56804112ef544ff91d7ca135beaa
Juri Pospelow <juri@pospelow.de>
2024-08-03 16:20:47 +0200

    webapi: use boost:property_tree for read ini file

--- src/WebServer.hpp
+++ src/WebServer.hpp
@@ -2,12 +2,21 @@
 
 #include <string>
 #include <map>
+#include <boost/asio.hpp>
+
+namespace net = boost::asio;                    // from <boost/asio.hpp>
 
 class WebServer {
+    net::ip::address _address{};
+    unsigned short _port {};
+    std::shared_ptr<std::string> _doc_root{};
+    int _threads{};
+
+    typedef std::multimap<std::string, std::string> MultiMap;
+    MultiMap DataPrepare;
 public:
     explicit WebServer(std::string fileName);
 
 private:
-    typedef std::multimap<std::string, std::string> MultiMap;
-    MultiMap DataPrepare;
+    void readCSV(std::string file_name);
 };
