88ea754461b099165bdf9987327df22464c82bc7
Juri Pospelow <juri@pospelow.de>
2024-08-03 13:52:15 +0200

    webapi: introduce Listener.cpp

---
+++ src/Listener.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <boost/beast/core/bind_handler.hpp>
+#include "Listener.hpp"
+#include "HTTPSession.hpp"
+
+namespace beast = boost::beast;                 // from <boost/beast.hpp>
+
+// Report a failure
+void listener::fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+listener::listener(
+        net::io_context& ioc,
+        tcp::endpoint endpoint,
+        std::shared_ptr<std::string const> const& doc_root)
+        : ioc_(ioc)
+        , acceptor_(net::make_strand(ioc))
+        , doc_root_(doc_root)
+{
+    beast::error_code ec;
+
+    // Open the acceptor
+    acceptor_.open(endpoint.protocol(), ec);
+    if(ec)
+    {
+        fail(ec, "open");
+        return;
+    }
+
+    // Allow address reuse
+    acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+    if(ec)
+    {
+        fail(ec, "set_option");
+        return;
+    }
+
+    // Bind to the server address
+    acceptor_.bind(endpoint, ec);
+    if(ec)
+    {
+        fail(ec, "bind");
+        return;
+    }
+
+    // Start listening for connections
+    acceptor_.listen(
+        net::socket_base::max_listen_connections, ec);
+    if(ec)
+    {
+        fail(ec, "listen");
+        return;
+    }
+}
+
+// Start accepting incoming connections
+void listener::run()
+{
+    // We need to be executing within a strand to perform async operations
+    // on the I/O objects in this session. Although not strictly necessary
+    // for single-threaded contexts, this example code is written to be
+    // thread-safe by default.
+    net::dispatch(
+        acceptor_.get_executor(),
+        beast::bind_front_handler(
+            &listener::do_accept,
+            this->shared_from_this()));
+}
+
+void listener::do_accept()
+{
+    // The new connection gets its own strand
+    acceptor_.async_accept(
+        net::make_strand(ioc_),
+        beast::bind_front_handler(
+            &listener::on_accept,
+            shared_from_this()));
+}
+
+void listener::on_accept(beast::error_code ec, tcp::socket socket)
+{
+    if(ec)
+    {
+        fail(ec, "accept");
+    }
+    else
+    {
+        // Create the http session and run it
+        std::make_shared<http_session>(
+            std::move(socket),
+            doc_root_)->run();
+    }
+
+    // Accept another connection
+    do_accept();
+}
---
+++ src/Listener.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+using tcp = boost::asio::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
+namespace net = boost::asio;                    // from <boost/asio.hpp>
+namespace beast = boost::beast;                 // from <boost/beast.hpp>
+
+// Accepts incoming connections and launches the sessions
+class listener : public std::enable_shared_from_this<listener>
+{
+    net::io_context& ioc_;
+    tcp::acceptor acceptor_;
+    std::shared_ptr<std::string const> doc_root_;
+
+public:
+    listener(
+        net::io_context& ioc,
+        tcp::endpoint endpoint,
+        std::shared_ptr<std::string const> const& doc_root);
+
+    // Start accepting incoming connections
+    void run();
+
+private:
+// Report a failure
+    void fail(beast::error_code ec, char const* what);
+    void do_accept();
+    void on_accept(beast::error_code ec, tcp::socket socket);
+};
